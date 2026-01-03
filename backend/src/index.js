e688c99dcbd7580806e64dfd4c14b2db6c14da3f
Raphael Nacarato <nacarato.raphael@gmail.com>
2026-01-03 11:56:11 -0300

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,17 @@
 /* eslint-disable linebreak-style */
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
-mongoose.connect('mongodb+srv://teste:teste@cluster0-37abp.mongodb.net/test?retryWrites=true&w=majority', {
+const connectURL = process.env.MONGODB_URL;
+
+mongoose.connect(connectURL, {
   useNewUrlParser: true,
 });
 
@@ -21,8 +23,11 @@ app.use((req, res, next) => {
 
 app.use(cors());
 
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+);
 
-app.use(require('./routes'));
+app.use(require("./routes"));
 
 server.listen(3333);
